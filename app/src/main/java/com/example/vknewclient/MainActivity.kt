d9550fe29ae55e2032e5a5b6045bed36891cf564
Kirill <kirill.pomaskin@gmail.com>
2023-09-19 16:06:16 +0300

    Creating app

---
+++ app/src/main/java/com/example/vknewclient/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.vknewclient
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.example.vknewclient.ui.theme.MainScreen
+import com.example.vknewclient.ui.theme.PostCard
+import com.example.vknewclient.ui.theme.VkNewClientTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
