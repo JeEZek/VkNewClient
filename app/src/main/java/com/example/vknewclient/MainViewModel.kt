7f50bb37ce10dcf557a1d1e3a1f72024727d9ee9
Kirill <kirill.pomaskin@gmail.com>
2023-10-03 20:50:44 +0300

    Add LazyColumn and SwipeToDismiss

--- app/src/main/java/com/example/vknewclient/MainViewModel.kt
+++ app/src/main/java/com/example/vknewclient/MainViewModel.kt
@@ -7,13 +7,20 @@ import com.example.vknewclient.domain.FeedPost
 import com.example.vknewclient.domain.StatisticItem
 
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
-    private val _feedPost = MutableLiveData(FeedPost())
-    val feedPost: LiveData<FeedPost> = _feedPost
+    private val initialList = mutableListOf<FeedPost>().apply {
+        repeat(10) {
+            add(FeedPost(id = it))
+        }
+    }
 
-    fun updateCount(item: StatisticItem) {
-        val oldStatistics = feedPost.value?.statistics ?: throw IllegalStateException()
+    private val _feedPosts = MutableLiveData<List<FeedPost>>(initialList)
+    val feedPosts: LiveData<List<FeedPost>> = _feedPosts
+
+    fun updateCount(feedPost: FeedPost, item: StatisticItem) {
+        val oldPosts = _feedPosts.value?.toMutableList() ?: mutableListOf()
+        val oldStatistics = feedPost.statistics
         val newStatistics = oldStatistics.toMutableList().apply {
             replaceAll { oldItem ->
                 if (oldItem.type == item.type) {
@@ -23,6 +30,21 @@ class MainViewModel: ViewModel() {
                 }
             }
         }
-        _feedPost.value = feedPost.value?.copy(statistics = newStatistics)
+        val newFeedPost = feedPost.copy(statistics = newStatistics)
+        _feedPosts.value = oldPosts.apply {
+            replaceAll {
+                if (it.id == newFeedPost.id) {
+                    newFeedPost
+                } else {
+                    it
+                }
+            }
+        }
+    }
+
+    fun removePost(feedPost: FeedPost) {
+        val modifiedList = _feedPosts.value?.toMutableList() ?: mutableListOf()
+        modifiedList.remove(feedPost)
+        _feedPosts.value = modifiedList
     }
 }
